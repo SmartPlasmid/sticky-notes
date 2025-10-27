12bee19615e523608e80797d267c810703cf3d66
Tony Dang <35576591+SmartPlasmid@users.noreply.github.com>
2025-10-28 00:10:31 +0100

    applied note-specific style as inline style to noteCard component

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const NoteCard = ( {note} ) => {
   const data = JSON.parse(note.body)
+  const colors = JSON.parse(note.colors)
   return (
-    <div>
+    <div style={{backgroundColor : colors.colorBody}}>
       {data}
     </div>
   )
